2a538197bd241755e5e36fcafac05b3b247630e6
JohnCosta27 <johncosta027@gmail.com>
2021-06-09 22:34:22 +0100

    Day 5 part 1

---
+++ day5/day5.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Day 5")
+	start := time.Now()
+
+	dat, err := ioutil.ReadFile("day5.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := bytes.Split(dat, []byte("\n"))
+	part1 := 0
+	part2 := 0
+
+	for _, value := range lines {
+
+		vowels := 0
+		twiceInARow := false
+		forbiddenPhrases := false
+
+		for i := 0; i < len(value); i++ {
+			current := value[i]
+			if current == []byte("a")[0] || current == []byte("e")[0] || current == []byte("i")[0] || current == []byte("o")[0] || current == []byte("u")[0] {
+				vowels++
+			}
+
+			if i != 0 {
+				if value[i-1] == value[i] {
+					twiceInARow = true
+				}
+
+				last2 := []byte{value[i-1], value[i]}
+				if compare(last2, []byte("ab")) || compare(last2, []byte("cd")) || compare(last2, []byte("pq")) || compare(last2, []byte("xy")) {
+					forbiddenPhrases = true
+					break
+				}
+
+			}
+
+		}
+
+		if !forbiddenPhrases {
+			if twiceInARow && vowels >= 3 {
+				part1++
+			}
+		}
+
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Part 1: %v %v \n", part1, elapsed)
+	fmt.Printf("Part 2: %v %v", part2, elapsed)
+
+}
+
+func compare(slice1 []byte, slice2 []byte) bool {
+	for i := 0; i < len(slice1) && i < len(slice2); i++ {
+		if slice1[i] != slice2[i] {
+			return false
+		}
+	}
+	return true
+}
